aa6b5191ad2a114c06c354842f1793862f5d2342
hilton <136855993+andrew-hilton-01@users.noreply.github.com>
2025-06-05 21:22:51 -0400

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import { inject } from "@vercel/analytics"
 import Main from './components/Main';
 import Background from './components/Background';
 import './App.css'
 import Sidebar from './components/Sidebar';
-
+inject();
 function App() {
 
 return (
